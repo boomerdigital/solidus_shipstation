3d102243c8ffeb760a2020c7ed46b31c38c00c60
Octave Zangs <octavez@octavez.com>
2019-03-12 11:35:24 -0700

    Don't fetch Easypost shipments

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,6 +1,11 @@
 Spree::Shipment.class_eval do
   def self.exportable
-    query = order(:updated_at).joins(:order).merge(Spree::Order.complete).where.not(spree_shipments: { state: 'canceled' })
+    easypost = Spree::StockLocation.find_by_name('Easypost')
+    if easypost.present?
+      query = order(:updated_at).joins(:order).merge(Spree::Order.complete).where.not(spree_shipments: { state: 'canceled', stock_location_id: easypost.id })
+    else
+      query = order(:updated_at).joins(:order).merge(Spree::Order.complete).where.not(spree_shipments: { state: 'canceled' })
+    end
     query = query.ready unless Spree::Config.shipstation_capture_at_notification
     query
   end
